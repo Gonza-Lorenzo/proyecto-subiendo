75a0c5922c7eeb2ed12c01ffac1d5fcf86318d51
Gonzalo Lorenzo <gonzalorenzo55@gmail.com>
2025-10-02 13:47:08 -0300

    Agregando cambios en main.js

--- src/main.js
+++ src/main.js
@@ -1,23 +1,138 @@
 import './style.css'
 
+console.warn('// ! Callbacks');
+// Una función que se pasa como argumento a otra función
 
 const texto1 = () => {
-  console.log("Soy el texto 1")
+    console.log("Soy el texto 1")
 }
 
-console.log(texto1); // REFERENCIA DE LA FUNCION -> Mostrando referencia de la funcion
-texto1() // Ejecuntando, invocando, llamando a la funcion
+console.log(texto1); //  REFERENCIA FUNCIÓN -> Mostrando la referencia de la función.
+texto1() // Ejecutando, invocando, llamando a la función
 
 const prueba = (callback) => {
-  console.log(callback) // La referencia de la funcion texto1
-  console.log('Otra cosa')
-  console.log('------')
-  console.log('------')
-  console.log('Otra cosa mas')
-  callback()
+    console.log(callback) // la referencia de la función texto1
+    console.log('Otra cosa')
+    console.log('------')
+    console.log('------')
+    console.log('Otra cosa más')
+    callback()
 }
 
 //prueba(texto1)
 
-console.warn('// ! Callbacks')
-// Una funcion que se pasa como argumento a otra funcion
+console.warn('// ! EVENTOS');
+
+// ! addEventListener()
+
+const btnClick = document.querySelector('#btn-click')
+console.log(btnClick)
+
+// btnClick.addEventListener('<tipo-evento>', callback)
+btnClick.addEventListener('click', function() {
+    console.log('Hicieron click')
+})
+
+btnClick.addEventListener('click', function() {
+    console.log('Otro evento. Hicieron click!')
+})
+
+// https://developer.mozilla.org/en-US/docs/Web/API/Event
+
+function v1() {
+    const btnUno = document.querySelector('#btn-1')
+    const btnDos = document.querySelector('#btn-2')
+    const btnTres = document.querySelector('#btn-3')
+    const btnCuatro = document.querySelector('#btn-4')
+
+    console.log(btnUno)
+    console.log(btnDos)
+    console.log(btnTres)
+    console.log(btnCuatro)
+
+    btnUno.addEventListener('click', () => {
+        btnUno.style.backgroundColor = 'crimson'
+    })
+    btnDos.addEventListener('click', () => {
+        btnDos.style.backgroundColor = 'gray'
+    })
+    btnTres.addEventListener('click', () => {
+        btnTres.style.backgroundColor = 'lightgreen'
+    })
+    btnCuatro.addEventListener('click', () => {
+        btnCuatro.style.backgroundColor = '#5f85eb'
+    })
+}
+
+// v1()
+
+function v2() {
+    const botones = document.querySelectorAll('div button')
+    
+    console.log(botones)
+
+    botones[0].addEventListener('click', () => {
+        botones[0].style.backgroundColor = 'crimson'
+    })
+    botones[1].addEventListener('click', () => {
+        botones[1].style.backgroundColor = 'gray'
+    })
+    botones[2].addEventListener('click', () => {
+        botones[2].style.backgroundColor = 'lightgreen'
+    })
+    botones[3].addEventListener('click', () => {
+        botones[3].style.backgroundColor = '#5f85eb'
+    })
+}
+
+// v2()
+
+function v3() {
+    const botones = document.querySelectorAll('div button')
+    
+    console.log(botones)
+
+    const arrayColores = ['crimson', 'rebeccapurple', 'darkblue', 'darkorange']
+
+    botones.forEach((boton, i) => {
+        boton.addEventListener('click', () => {
+            boton.style.backgroundColor = arrayColores[i]
+        })
+    });
+}
+
+//v3()
+
+function v4() {
+
+    //const botones = document.querySelectorAll('div button')
+    //console.log(botones)
+    const contenedor = document.querySelector('div')
+    console.log(contenedor)
+    // El addEventListener -> Le entregan al callback un objeto que representa el evento.
+    contenedor.addEventListener('click', function(evento) {
+        // console.log(evento) // e, evt, event, evento.
+        // target dentro del objeto evento representa al elemento que disparo el evento.
+        //console.log(evento)
+
+        if ( evento.target.localName !== 'div' ) {
+            console.log('Hicieron click en algún botón')
+
+            evento.target.style.backgroundColor = evento.target.dataset.color || 'gold'
+
+            /* console.dir(evento.target) */
+
+        } 
+
+        // Propagación de eventos...
+
+        /* if ( evento.target.localName === 'div' ) {
+            console.log('Hicieron click en el div')
+        } else {
+            console.log('Hicieron click en algún botón')
+        } */
+
+    })
+}
+
+v4()
